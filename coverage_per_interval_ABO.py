dc9f2a1968d01e6258bfdb676c4e27df2d4ae5fc
stevens-j <jstevens81@gmail.com>
2021-05-12 10:16:02 -0400

    added function comments and edited function names

--- coverage_per_interval_ABO.py
+++ coverage_per_interval_ABO.py
@@ -59,16 +59,24 @@ def process_files_from_list(file_list):
     sample_list = list()
     depth_dict = dict()
     for file in file_list:
+        # append sample name to sample_list
         sample_name = file.split('_')[0]
         sample_list.append(sample_name)
+        # specify path to individual depth file
         basepath = '/Users/jonathan_stevens/ABO/1000G_data/depth/'
         file_path = os.path.join(basepath, file)
+        # create file handle for depth file
         file_handle = open(file_path, 'r')
+        # extract depth data from file
         sample_depth_list = _get_depth_list(file_handle)
         file_handle.close()
-        binned_depth_list = _bin_depth_list(sample_depth_list)
-        avg_depth_per_bin = _avg_depth_per_bin(binned_depth_list)
-        depth_dict[sample_name] = avg_depth_per_bin
+        # split depth data into 100 base interval lists
+        depth_intervals_list = _create_depth_intervals_list(sample_depth_list)
+        # calculate average value for each interval
+        avg_depth_per_interval = \
+            _calculate_avg_depth_per_interval(depth_intervals_list)
+        # add average depth per interval to dictionary
+        depth_dict[sample_name] = avg_depth_per_interval
     return sample_list, depth_dict
 
 
@@ -82,29 +90,32 @@ def _get_depth_list(file_handle):
     sample_depth_list = list()
     for line in file_handle:
         line = line.split()
+        # create list of depth values from column 3 of file
         sample_depth_list.append(int(line[2]))
     return sample_depth_list
 
 
-def _bin_depth_list(sample_depth_list):
+def _create_depth_intervals_list(sample_depth_list):
     """
     Splits the depth list into 100 element lists
     :param sample_depth_list: list of depth values
     :return: list of 100 element lists
     """
     depth_intervals_list = list()
+    # divide list into 100 element lists
     for i in range(0, len(sample_depth_list), 100):
         depth_intervals_list.append(sample_depth_list[i:i + 100])
     return depth_intervals_list
 
 
-def _avg_depth_per_bin(depth_intervals_list):
+def _calculate_avg_depth_per_interval(depth_intervals_list):
     """
     Calculates the average depth per interval
     :param depth_intervals_list: list of depths per 100 bases
     :return: avg_depth_per_interval, list of average depths per 100 bases
     """
     avg_depth_per_interval = list()
+    # calculate average of each interval
     for interval in depth_intervals_list:
         avg_depth_per_interval.append(round(sum(interval) / len(interval)))
     return avg_depth_per_interval
@@ -117,6 +128,7 @@ def create_file_headers():
     """
     header_line = list()
     header_line.append("Sample")
+    # generate 100 base interval column headers
     for i in range(133255176, 133385146, 100):
         header_line.append(f'chr9:{i}')
     return header_line
@@ -133,8 +145,10 @@ def print_data_2_file(out_file, header_line, sample_list, depth_dict):
     :return: None
     """
     with open(out_file, 'w') as fh:
+        # write tab delimited header line to file
         header_line_joined = "\t".join(header_line)
         fh.write(f'{header_line_joined}\n')
+        # write sample data per line to file
         for sample in sample_list:
             coverage = '\t'.join(str(i) for i in depth_dict[sample])
             sample_line = '\t'.join((sample, coverage))
